6d707bdef32fb648e7feb5b7252b3778f28061be
Nazir <anwernazir674@gmail.com>
2023-11-14 13:12:47 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Home from './screens/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hiiiii World</h1>
+    <div >
+      <div><Home/></div>
     </div>
   );
 }
